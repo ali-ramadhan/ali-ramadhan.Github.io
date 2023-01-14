6809e7b4c3bd53fe8b159d66f2349c88ff49c590
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2023-01-13 20:41:41 -0500

    More functional video sphere

--- video-sphere.js
+++ video-sphere.js
@@ -2,8 +2,10 @@
 
 import * as THREE from "https://cdn.skypack.dev/three@0.136.0";
 import {
-  OrbitControls
+    OrbitControls
 } from "https://cdn.skypack.dev/three@0.136.0/examples/jsm/controls/OrbitControls.js";
+
+// three.js setup
 
 let scene = new THREE.Scene();
 
@@ -14,26 +16,18 @@ let camera = new THREE.PerspectiveCamera(60, video_width / video_height, 1, 1000
 camera.position.set(0, 0, 10);
 
 let renderer = new THREE.WebGLRenderer({
-  antialias: true,
-  alpha: true
+    antialias: true,
+    alpha: true
 });
 
 renderer.setSize(video_width, video_height);
-// renderer.setClearColor(0x404040);
 
-let video_sphere_box = document.getElementById('video-sphere-box');
-video_sphere_box.appendChild(renderer.domElement);
+let video_sphere_box = document.getElementById('video-inner-box');
+let canvasElement = video_sphere_box.appendChild(renderer.domElement);
+canvasElement.setAttribute("id", "canvas-video-sphere");
 
 let controls = new OrbitControls(camera, renderer.domElement);
-
-btnPlay.addEventListener("click", event => {
-	btnPlay.style.display = "none";
-  let video = document.getElementById('video');
-  video.play();
-  let videoTex = new THREE.VideoTexture(video);
-  m.map = videoTex;
-  m.needsUpdate = true;
-})
+controls.enableZoom = false;
 
 let g = new THREE.SphereGeometry(5, 128, 64);
 g.rotateY(-0.5 * Math.PI);
@@ -46,13 +40,68 @@ window.addEventListener("resize", onWindowResize);
 animate();
 
 function animate() {
-  requestAnimationFrame( animate );
-  renderer.render(scene, camera);
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
 }
 
 function onWindowResize() {
-  camera.aspect = video_width / video_height;
-  camera.updateProjectionMatrix();
+    camera.aspect = video_width / video_height;
+    camera.updateProjectionMatrix();
 
-  renderer.setSize(video_width, video_height);
+    renderer.setSize(video_width, video_height);
 }
+
+// dat.gui setup
+
+var options = {
+    playback_rate: 1.0,
+};
+
+var gui = new dat.GUI();
+
+var video_options = gui.addFolder('Video');
+video_options.add(options, 'playback_rate', 0.0, 5.0, 0.1).name('Playback rate')
+    .onChange(
+        function () {
+            let video = document.getElementById('video');
+            video.playbackRate = options.playback_rate;
+        }
+    )
+
+video_options.open();
+
+// video sphere setup
+
+let video = document.getElementById('video');
+video.play();
+video.defaultPlaybackRate = 1.0;
+video.playbackRate = options.playback_rate;
+
+let videoTex = new THREE.VideoTexture(video);
+m.map = videoTex;
+m.needsUpdate = true;
+
+let videoTitle = document.getElementById("video-title");
+let videoCaption = document.getElementById("video-caption");
+
+// Saved all videos at 24 FPS so that 1 second video time = 1 day simulation time.
+const framerate = 24;
+
+video.addEventListener('timeupdate', function () {
+    let frameNum = Math.floor(framerate * video.currentTime);
+    videoCaption.innerHTML = `Frame ${frameNum}`;
+});
+
+let video_sources = {
+    'precipitation': "https://raw.githubusercontent.com/ali-ramadhan/artifact-sandbox/main/precip.webm",
+    'temperature': "https://raw.githubusercontent.com/ali-ramadhan/artifact-sandbox/main/temperature.mp4"
+}
+
+let videoSelect = document.getElementById('video-select');
+
+videoSelect.addEventListener('change', (event) => {
+    video.pause();
+    videoTitle.innerHTML = videoSelect.value;
+    video.src = video_sources[videoSelect.value]
+    video.play();
+});

252ec6c04fcef20fac1ddff13b1d939258b80657
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2023-02-06 18:30:05 -0500

    Cleaner script and tooltips

--- video-sphere.js
+++ video-sphere.js
@@ -4,6 +4,67 @@ import * as THREE from "https://cdn.skypack.dev/three@0.136.0";
 import {OrbitControls} from "https://cdn.skypack.dev/three@0.136.0/examples/jsm/controls/OrbitControls.js";
 
 /////
+///// Data
+/////
+
+let videos = [
+    {
+        "title": "Temperature",
+        "src": "https://raw.githubusercontent.com/ali-ramadhan/artifact-sandbox/main/temperature.mp4",
+        "tooltips": {
+            "Boston": {
+                "latitude": 42.3601,
+                "longitude": -71.0589,
+                "color": "purple",
+                "title": "Tooltip title goes here",
+                "text": "<b>Lorem ipsum</b> dolor sit amet, <i>consectetur adipiscing elit</i>, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            },
+            "Vegas": {
+                "latitude": 36.1716,
+                "longitude": -115.1391,
+                "color": "red",
+                "title": "Second tooltip title goes here",
+                "text": "<b>Lorem ipsum</b> dolor sit amet, <i>consectetur adipiscing elit</i>, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
+            }
+        }
+    },
+
+    {
+        "title": "Precipitation",
+        "src": "https://raw.githubusercontent.com/ali-ramadhan/artifact-sandbox/main/precip.webm"
+    }
+]
+
+// Saved all videos at 24 FPS so that 1 second video time = 1 day simulation time.
+const videoFramerate = 24;
+
+/////
+///// Utils
+/////
+
+// Proper modulo function. % is actually the remainder function.
+function mod(n, m) {
+    return ((n % m) + m) % m;
+}
+
+function deg2rad(theta) {
+    return theta * (Math.PI / 180);
+}
+
+function sind(theta) {
+    return Math.sin(deg2rad(theta));
+}
+
+function cosd(theta) {
+    return Math.cos(deg2rad(theta));
+}
+
+Date.prototype.addHours = function(h) {
+    this.setTime(this.getTime() + (h*60*60*1000));
+    return this;
+}
+
+/////
 ///// three.js setup
 /////
 
@@ -90,31 +151,12 @@ sphereMeshMaterial.needsUpdate = true;
 let videoTitle = document.getElementById("video-title");
 let videoCaption = document.getElementById("video-caption");
 
-// Saved all videos at 24 FPS so that 1 second video time = 1 day simulation time.
-const framerate = 24;
-
-Date.prototype.addHours = function(h) {
-    this.setTime(this.getTime() + (h*60*60*1000));
-    return this;
-}
-
 video.addEventListener('timeupdate', function () {
     let dateStart = new Date("2015-08-01T00:00:00");
-    let frameNum = Math.floor(framerate * video.currentTime);
+    let frameNum = Math.floor(videoFramerate * video.currentTime);
     let dateFrame = dateStart.addHours(frameNum);
     videoCaption.textContent = `${dateFrame.toISOString().slice(0, -8)}Z`;
 });
-
-let videos = [
-    {
-        "title": "Temperature",
-        "src": "https://raw.githubusercontent.com/ali-ramadhan/artifact-sandbox/main/temperature.mp4"
-    },
-    {
-        "title": "Precipitation",
-        "src": "https://raw.githubusercontent.com/ali-ramadhan/artifact-sandbox/main/precip.webm"
-    }
-]
 
 let currentVideo = 0;
 
@@ -128,11 +170,6 @@ function selectVideo(n) {
 let leftArrow = document.getElementById('left-arrow');
 let rightArrow = document.getElementById('right-arrow');
 
-// Proper modulo function. % is actually the remainder function.
-function mod(n, m) {
-    return ((n % m) + m) % m;
-}
-
 leftArrow.addEventListener('click', (event) => {
     currentVideo = mod(currentVideo - 1, videos.length);
     selectVideo(currentVideo);
@@ -143,68 +180,53 @@ rightArrow.addEventListener('click', (event) => {
     selectVideo(currentVideo);
 });
 
-function deg2rad(theta) {
-    return theta * (Math.PI / 180);
-}
-
-function sind(theta) {
-    return Math.sin(deg2rad(theta));
-}
-
-function cosd(theta) {
-    return Math.cos(deg2rad(theta));
-}
-
-// The north pole of the three.js sphere corresponds to 0, 0 degrees.
-// So we apply +90 degree rotation about the x-axis.
+// Convert geographical (lat, lon) corresponds to (x, y, z) three.js coordinates on our sphere.
 function latlon2xyz(lat, lon) {
     let x = sind(90 - lat) * cosd(-lon);
     let y = sind(90 - lat) * sind(-lon);
     let z = cosd(90 - lat);
-
-    let x2 = x;
-    let y2 = z;
-    let z2 = y;
     
-    return [x2, y2, z2];
+    // Switching y and z corresponds to a +90 degree rotation around the x-axis.
+    return [x, z, y];
 }
 
-let lat_BOS = 42; // 42.3601;
-let lon_BOS = -71; // -71.0589;
+let tooltipMeshGroup = new THREE.Group();
 
-let [x_BOS, y_BOS, z_BOS] = latlon2xyz(lat_BOS, lon_BOS);
+for (const [name, tooltip] of Object.entries(videos[0]["tooltips"])) {
+    let tooltipLatitude = tooltip["latitude"];
+    let tooltipLongitude = tooltip["longitude"];
 
-console.log(`BOS = (lat=${lat_BOS}, lon=${lon_BOS}) -> (x=${x_BOS}, y=${y_BOS}, z=${z_BOS})`)
+    let [tooltipX, tooltipY, tooltipZ] = latlon2xyz(tooltipLatitude, tooltipLongitude);
 
-// const axesHelper = new THREE.AxesHelper(2);
-// scene.add(axesHelper);
+    let tooltipGeometry = new THREE.CircleGeometry(0.05, 25);
+    let tooltipMaterial = new THREE.MeshBasicMaterial({color: tooltip["color"], transparent: true, opacity: 0.5});
+    let tooltipMesh = new THREE.Mesh(tooltipGeometry, tooltipMaterial);
 
-const _geo = new THREE.CircleGeometry(0.05, 25);
-const _mat = new THREE.MeshBasicMaterial({color: 0xFF0000, transparent: true, opacity: 0.5});
-const _mesh = new THREE.Mesh(_geo, _mat);
-_mesh.position.set(x_BOS, y_BOS, z_BOS);
-_mesh.lookAt(1.1*x_BOS, 1.1*y_BOS, 1.1*z_BOS);
-scene.add(_mesh);
+    tooltipMesh.name = name;
+    tooltipMesh.position.set(tooltipX, tooltipY, tooltipZ);
+    tooltipMesh.lookAt(1.1*tooltipX, 1.1*tooltipY, 1.1*tooltipZ); // Make the circle flat on the sphere.
 
-const mousePos = new THREE.Vector2();
+    tooltipMeshGroup.add(tooltipMesh);
+}
+
+scene.add(tooltipMeshGroup);
+
 const raycaster = new THREE.Raycaster();
 
 window.addEventListener("mousemove", function(e) {
+    let mousePosition = new THREE.Vector2();
     let canvasBounds = document.getElementById("canvas-video-sphere").getBoundingClientRect();
-    mousePos.x = ((e.clientX - canvasBounds.left) / (canvasBounds.right - canvasBounds.left)) * 2 - 1;
-    mousePos.y = -((e.clientY - canvasBounds.top) / (canvasBounds.bottom - canvasBounds.top)) * 2 + 1;
+    mousePosition.x = ((e.clientX - canvasBounds.left) / (canvasBounds.right - canvasBounds.left)) * 2 - 1;
+    mousePosition.y = -((e.clientY - canvasBounds.top) / (canvasBounds.bottom - canvasBounds.top)) * 2 + 1;
 
-    // console.log(`client = (x=${e.clientX}, y=${e.clientY}}) | mousePos = (x=${mousePos.x}, y=${mousePos.y})`)
-
-    raycaster.setFromCamera(mousePos, camera);
-
-    const intersects = raycaster.intersectObject(_mesh);
-
-    // console.log(`intersecting ${intersects.length} things`);
+    raycaster.setFromCamera(mousePosition, camera);
+    let intersects = raycaster.intersectObject(tooltipMeshGroup);
 
     let tooltip = document.getElementById("video-tooltip");
 
     if (intersects.length > 0) {
+        let tooltipName = intersects[0].object.name;
+
         tooltip.style.zIndex = 10;
         tooltip.style.display = "inline-block";
         tooltip.style.position = "absolute";
@@ -216,8 +238,8 @@ window.addEventListener("mousemove", function(e) {
         tooltip.style.padding = "0.3em";
         tooltip.style.width = "400px";
 
-        let tooltipTitle = "Tooltip title goes here";
-        let tooltipText = "<b>Lorem ipsum</b> dolor sit amet, <i>consectetur adipiscing elit</i>, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+        let tooltipTitle = videos[0]["tooltips"][tooltipName]["title"];
+        let tooltipText = videos[0]["tooltips"][tooltipName]["text"];
 
         let tooltipTitleDiv = document.createElement("div");
         let tooltipTextDiv = document.createElement("div");

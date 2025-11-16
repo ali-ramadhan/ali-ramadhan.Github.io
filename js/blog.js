cc7506b2948ea90a98dc8ff699868b506282f05c
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2025-11-16 09:10:05 -0700

    Properly import Prism themes

--- js/blog.js
+++ js/blog.js
@@ -27,6 +27,11 @@ import "prismjs/plugins/toolbar/prism-toolbar";
 import "prismjs/plugins/show-language/prism-show-language";
 import "prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard";
 
+// Import Prism CSS (Vite will bundle these)
+import "prism-themes/themes/prism-one-dark.css";
+import "prismjs/plugins/line-numbers/prism-line-numbers.css";
+import "prismjs/plugins/toolbar/prism-toolbar.css";
+
 // Initialize minimal functionality for blog pages
 new ThemeManager();
 new MathJaxManager();
